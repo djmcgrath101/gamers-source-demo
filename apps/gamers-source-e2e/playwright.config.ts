c21afb29f9b4067d38564708242d02311a76086b
Daniel McGrath <djmcgrath@cybertec.io>
2026-05-14 16:56:00 -0500

    ci: add lint-and-test-affected actions workflow

--- apps/gamers-source-e2e/playwright.config.ts
+++ apps/gamers-source-e2e/playwright.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig, devices } from '@playwright/test';
-import { nxE2EPreset } from '@nx/playwright/preset';
 import { workspaceRoot } from '@nx/devkit';
+import { nxE2EPreset } from '@nx/playwright/preset';
+import { defineConfig, devices } from '@playwright/test';
 
 // For CI, you may want to set BASE_URL to the deployed application.
 const baseURL = process.env['BASE_URL'] || 'http://localhost:4200';
@@ -20,30 +20,20 @@ export default defineConfig({
   use: {
     baseURL,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on-first-retry'
   },
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'pnpm exec nx run gamers-source:serve',
     url: 'http://localhost:4200',
     reuseExistingServer: true,
-    cwd: workspaceRoot,
+    cwd: workspaceRoot
   },
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    },
-
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
-
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+      use: { ...devices['Desktop Chrome'] }
+    }
 
     // Uncomment for mobile browsers support
     /* {
@@ -64,5 +54,5 @@ export default defineConfig({
       name: 'Google Chrome',
       use: { ...devices['Desktop Chrome'], channel: 'chrome' },
     } */
-  ],
+  ]
 });
