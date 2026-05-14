bd514c6a607287d2acd6fab56dd3117a1f6b10ad
Daniel McGrath <djmcgrath@cybertec.io>
2026-05-14 17:24:57 -0500

    chore(gamers-source): remove boilerplate

--- apps/gamers-source/src/app/app.ts
+++ apps/gamers-source/src/app/app.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcome } from './nx-welcome';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [],
   selector: 'gs-root',
   templateUrl: './app.html',
-  styleUrl: './app.scss',
+  styleUrl: './app.scss'
 })
-export class App {
-  protected title = 'gamers-source';
-}
+export class App {}
