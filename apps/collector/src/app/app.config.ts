3905a7e22cadbb68459a219156d38d4ac5b92b37
Daniel McGrath <djmcgrath@cybertec.io>
2026-05-16 09:58:58 -0500

    chore(collector): setup providers & not found route

--- apps/collector/src/app/app.config.ts
+++ apps/collector/src/app/app.config.ts
@@ -1,17 +1,38 @@
-import { ApplicationConfig, isDevMode, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
+import {
+  ApplicationConfig,
+  isDevMode,
+  provideBrowserGlobalErrorListeners,
+  provideZonelessChangeDetection
+} from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { provideServiceWorker } from '@angular/service-worker';
+import { provideAppConfig, provideLogger } from '@gamers-source/shared-angular-core';
 import { appRoutes } from './app.routes';
+
+const appCode = 'GSC';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
+    provideZonelessChangeDetection(),
+    provideHttpClient(withFetch()),
+    provideClientHydration(withEventReplay()),
     provideRouter(appRoutes),
     provideServiceWorker('ngsw-worker.js', {
       enabled: !isDevMode(),
       registrationStrategy: 'registerWhenStable:30000'
-    })
+    }),
+
+    /**
+     * Lib providers
+     */
+    provideAppConfig({
+      name: "Gamer's Source Collector",
+      code: appCode,
+      theme: { active: 'system', themes: ['light', 'dark'] }
+    }),
+    provideLogger(appCode)
   ]
 };
--- apps/collector/src/app/app.routes.ts
+++ apps/collector/src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Route } from '@angular/router';
+import { PageNotFoundView } from '@gamers-source/shared-angular-core';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  // IMPORTANT: leave this route last
+  {
+    path: '**',
+    pathMatch: 'full',
+    component: PageNotFoundView
+  }
+];
